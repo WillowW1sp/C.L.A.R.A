c7a55c5aa16dea823dcf3ffb6b45a71753122c5b
WillowWisp <zcrawshaw@gmail.com>
2024-12-06 22:43:54 +0000

    modified:   Main/LLM/LMM.rb 	modified:   Main/installdependancies.rb

--- Main/installdependancies.rb
+++ Main/installdependancies.rb
@@ -1,1 +1,18 @@
-require "OS.rb"
+require "rbconfig"
+include Config
+
+case CONFIG["host_os"]
+when /mswin|windows/i
+# Windows
+
+when /linux/i
+# Linux
+sudo apt-get install mpg123 #for debain based
+when /sunos|solaris/i
+# Solaris
+else
+# Whatever
+end
+
+# convert to this instead: https://github.com/djberg96/sys-uname/wiki
+
